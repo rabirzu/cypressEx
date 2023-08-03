42fefe4eb0440747b0c8fcf3f281857726b55256
Raluca Birzu <rabirzu@deloittece.com>
2023-08-03 19:52:27 +0300

    section7

---
+++ cypress/integration/examples/Test6.js
@@ -0,0 +1,10 @@
+describe("Section 7", function () {
+  it("Section 7 exercises", function () {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+   // cy.get('div.mouse-hover-content').invoke('show')
+    cy.contains('Top').click({force:true})
+    cy.url().should('include','top')
+    
+  });
+});
