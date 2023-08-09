6cb098adb4d080c6aa0d2e1ff79cdbd75d97bdce
Raluca Birzu <rabirzu@deloittece.com>
2023-08-09 16:51:33 +0300

    integrations cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "cypress";
+const preprocessor = require("badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 export default defineConfig({
   projectId: "xq62ky",
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+      await preprocessor.addCucumberPreprocesorPlugin(on, config);
+      on("file:preprocessor", browserify.default(config));
     },
-    specPattern: "cypress/integration/examples/*.js",
+    specPattern: "cypress/integration/examples/BDD*.feature",
   },
 
   env: {
